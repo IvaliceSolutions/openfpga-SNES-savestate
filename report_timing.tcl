11d17d8ce14417d291513ed1a36e59f743992dd7
IvaliceSolutions <jh@ivalice.eu>
2026-06-30 22:31:37 +0200

    ci: write worst_paths.rpt to absolute /build path (project_open changes cwd)

--- report_timing.tcl
+++ report_timing.tcl
@@ -4,5 +4,5 @@ project_open projects/snes_pocket.qpf
 create_timing_netlist -model slow
 read_sdc
 update_timing_netlist
-report_timing -setup -npaths 15 -detail full_path -file projects/output_files/worst_paths.rpt
+report_timing -setup -npaths 15 -detail full_path -file /build/projects/output_files/worst_paths.rpt
 project_close
